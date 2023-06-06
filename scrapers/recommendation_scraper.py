749c65609ca9942021329ba316ee6f33c6d8a304
Sourcery AI <>
2023-06-06 10:27:49 +0000

    'Refactored by Sourcery'

--- scrapers/recommendation_scraper.py
+++ scrapers/recommendation_scraper.py
@@ -9,45 +9,43 @@ from pprint import pprint
 
 base_url = "https://api.themoviedb.org/3/movie/"
 
-json_file = open('../data/recommendation_metadata.json', 'a')
+with open('../data/recommendation_metadata.json', 'a') as json_file:
+	beginning = int(input("Enter Opening Page: "))
+	end = int(input("Enter Closing Page: "))
 
-beginning = int(input("Enter Opening Page: "))
-end = int(input("Enter Closing Page: "))
+	with open('data/links.csv', 'r') as f:
+		for count,line in enumerate(f):
+			if count > beginning and count <= end:
+				line = line.split(',')
+				tmdbId = line[2]
+				if len(tmdbId) == 0:
+					print("Movie doesn't exist is TMDB Database.")
+					continue
+				url = base_url + tmdbId + "/recommendations"  + "?api_key=" + API_KEY
 
-with open('data/links.csv', 'r') as f:
-	for count,line in enumerate(f):
-		if count > beginning and count <= end:
-			line = line.split(',')
-			tmdbId = line[2]
-			if len(tmdbId) == 0:
-				print("Movie doesn't exist is TMDB Database.")
-				continue
-			url = base_url + tmdbId + "/recommendations"  + "?api_key=" + API_KEY
-
-			print("Requesting Movie Number: " + str(count))
-			res = requests.get(url)
-
-			if res.status_code == 404:
-				print("Movie doesn't exist is TMDB Database.")
-				continue
-			elif res.status_code == 500:
-				print("Internal Server Error.")
-				continue
-			elif res.status_code == 429:
-				print("Time out. Waiting for 10 seconds")
-				time.sleep(10)
+				print(f"Requesting Movie Number: {str(count)}")
 				res = requests.get(url)
-			else:
-				res.raise_for_status()
 
-			js = dict(json.loads(res.text))
-			js["id"] = tmdbId
-			json_file.write(str(json.dumps(js)))
-			#pprint(json.loads(res.text))
+				if res.status_code == 404:
+					print("Movie doesn't exist is TMDB Database.")
+					continue
+				elif res.status_code == 500:
+					print("Internal Server Error.")
+					continue
+				elif res.status_code == 429:
+					print("Time out. Waiting for 10 seconds")
+					time.sleep(10)
+					res = requests.get(url)
+				else:
+					res.raise_for_status()
+
+				js = dict(json.loads(res.text))
+				js["id"] = tmdbId
+				json_file.write(json.dumps(js))
+						#pprint(json.loads(res.text))
 
 
 
-json_file.write(' ]')
-json_file.close()
+	json_file.write(' ]')
 
 
--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -9,39 +9,37 @@ from pprint import pprint
 
 base_url = "https://api.themoviedb.org/3/movie/"
 
-json_file = open('../data/movies_metadata.json', 'a')
+with open('../data/movies_metadata.json', 'a') as json_file:
+	data = []
 
-data = []
+	with open('data/links.csv', 'r') as f:
+		for count,line in enumerate(f):
+			if count > 0:
+				line = line.split(',')
+				tmdbId = line[2]
+				if len(tmdbId) == 0:
+					print("Movie doesn't exist is TMDB Database.")
+					continue
+				url = base_url + tmdbId + "?api_key=" + API_KEY
 
-with open('data/links.csv', 'r') as f:
-	for count,line in enumerate(f):
-		if count > 0:
-			line = line.split(',')
-			tmdbId = line[2]
-			if len(tmdbId) == 0:
-				print("Movie doesn't exist is TMDB Database.")
-				continue
-			url = base_url + tmdbId + "?api_key=" + API_KEY
-
-			print("Requesting Movie Number: " + str(count))
-			res = requests.get(url)
-
-			if res.status_code == 404:
-				print("Movie doesn't exist is TMDB Database.")
-				continue
-			elif res.status_code == 429:
-				print("Time out. Waiting for 10 seconds")
-				time.sleep(10)
+				print(f"Requesting Movie Number: {str(count)}")
 				res = requests.get(url)
-			else:
-				res.raise_for_status()
 
-			json_file.write(str(res.text) + ", ")
-			data.append(res.text)
-			#pprint(json.loads(res.text))
+				if res.status_code == 404:
+					print("Movie doesn't exist is TMDB Database.")
+					continue
+				elif res.status_code == 429:
+					print("Time out. Waiting for 10 seconds")
+					time.sleep(10)
+					res = requests.get(url)
+				else:
+					res.raise_for_status()
+
+				json_file.write(f"{res.text}, ")
+				data.append(res.text)
+						#pprint(json.loads(res.text))
 
 
-json_file.write(' ]')
-json_file.close()
+	json_file.write(' ]')
 
 
